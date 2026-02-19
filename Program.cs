3b7c91e648486b0f8549c8075bdd37b04d4fd9b9
NikJst <nikitazestovski@gmail.com>
2026-02-19 09:48:17 +0700

    подключение openAPI для работы с Post

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using Testing3;
-
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddScoped<IGuestService, GuestService>();
@@ -15,9 +19,13 @@ builder.Services.AddScoped<ILikeRepository, LikeRepositoryMock>();
 var app = builder.Build();
 
 
-app.Urls.Add("http://0.0.0.0:3002");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Urls.Add("http://0.0.0.0:3000");
 
 app.UseDefaultFiles();
+
 app.UseStaticFiles();
 
 app.MapControllers();
